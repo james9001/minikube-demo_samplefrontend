50f968d189ecbf92086866c39b01f37a3408b689
James Osgerby <james.osgerby@checkbox.ai>
2024-10-02 18:59:48 +1000

    improve build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo docker build -t localhost:5000/minikube-demo_samplefrontend:latest .
+sudo docker build -t hostmachine:5000/minikube-demo_samplefrontend:latest .
+sudo docker push hostmachine:5000/minikube-demo_samplefrontend:latest
